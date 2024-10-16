57784a220e5ad1b4f924fb84dd39ab3960d90ae6
jajerrr <107903870+jajerrr@users.noreply.github.com>
2024-10-16 17:56:24 +0700

    format

--- components/Home/zone6.vue
+++ components/Home/zone6.vue
@@ -5,131 +5,92 @@
       <div class="flex-glow j-center w-full px-[10%]">
         <h class="th-header">วิทยาลัยสร้างสรรค์</h>
       </div>
-    
-        <img src="assets/images/zone2/underline.png" class=" normal_model w-[30vw]" />
-
-        <img src="assets/images/zone2/underline.png" class=" small_model w-[20rem]" />
-
+      <img src="assets/images/zone2/underline.png" class=" normal_model w-[30vw]" />
+      <img src="assets/images/zone2/underline.png" class=" small_model w-[20rem]" />
     </div>
     <div class="flex-container p-[5%]">
       <!-- Main Swiper -->
       <Swiper @swiper="setSwiperRef" class="main-swiper w-1-2-center h-auto container-z6 "
-        style="height: 50%; margin-left: 10%; margin-top: 5vw; margin-right: 5%;" 
-        :modules="[Thumbs , EffectFade]" 
-        :effect="'fade'"
-        :slidesPerView="1"
-        :allowTouchMove="false"
-        :fadeEffect="{ crossFade: true }" 
+        style="height: 50%; margin-left: 10%; margin-top: 5vw; margin-right: 5%;" :modules="[Thumbs, EffectFade]"
+        :effect="'fade'" :slidesPerView="1" :allowTouchMove="false" :fadeEffect="{ crossFade: true }"
         :options="swiperOptions">
-        
-        <SwiperSlide class="card-container p-[10%]" 
-                     v-for="(card, index) in detailCard" 
-                     :key="index"
-                     style="transition: transform 2s ease, opacity 0s ease;">
+        <SwiperSlide class="card-container p-[10%]" v-for="(card, index) in detailCard" :key="index"
+          style="transition: transform 2s ease, opacity 0s ease;">
           <div>
             <div class="carder-header-z6">{{ card.header }}</div>
             <div class="card-desc-z6 scrollable-text">{{ card.desc }}</div>
-            <button type="button"
-  @click="openModel(card.models, card.header)"  
-  class="f-center button-orange SF-TH text-[13px] px-[3%] py-[1%]">
-  ดูแบบ360°
-</button>
+            <button type="button" @click="openModel(card.models, card.header)"
+              class="f-center button-orange SF-TH text-[13px] px-[3%] py-[1%]">
+              ดูแบบ360°
+            </button>
           </div>
         </SwiperSlide>
       </Swiper>
-
       <!-- Thumbs Swiper -->
-<Swiper ref="swiperRef1" class="thumbs-swiper w-1-2-center h-auto" style="width: 90%; height: 50vh;"
-  :modules="[Navigation, Thumbs]" :allowTouchMove="false" :centeredSlides="true" :slidesPerView="1"
-  :spaceBetween="10" :navigation="true" :thumbs="{ swiper: swiperRef2 }" :options="swiperOptions">
+      <Swiper ref="swiperRef1" class="thumbs-swiper w-1-2-center h-auto" style="width: 90%; height: 50vh;"
+        :modules="[Navigation, Thumbs]" :allowTouchMove="false" :centeredSlides="true" :slidesPerView="1"
+        :spaceBetween="10" :navigation="true" :thumbs="{ swiper: swiperRef2 }" :options="swiperOptions">
 
-  <SwiperSlide class="i-center" v-for="(card, index) in detailCard" :key="index"
-    style="transition: transform 5s ease, opacity 4.5s ease;">
-    
-    <div>
-   
-<img 
-        :src="getBodyImageSrc(card.img)" 
-        class="normal_model"
-        alt="body"
-      />
-      <img 
-        :src="getBodyImageSrc(card.img)" 
-        class="small_model img-sm-z6"
-        alt="body"
-      />
-    </div>
-  </SwiperSlide>
-</Swiper>
+        <SwiperSlide class="i-center" v-for="(card, index) in detailCard" :key="index"
+          style="transition: transform 5s ease, opacity 4.5s ease;">
+
+          <div>
+
+            <img :src="getBodyImageSrc(card.img)" class="normal_model" alt="body" />
+            <img :src="getBodyImageSrc(card.img)" class="small_model img-sm-z6" alt="body" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
     <img src="/images/OBJECTS6.png" class="normal_model image-full-ab top-[-25%] left-[-5%] z-[-1]" style="
     width: 25vw;
    " />
-   <img src="/images/OBJECTS6.png" class="small_model image-full-ab top-[-15%] left-[-5%] z-[-1]" style="
+    <img src="/images/OBJECTS6.png" class="small_model image-full-ab top-[-15%] left-[-5%] z-[-1]" style="
     width: 50vw;
    " />
-   <img src="/images/vector6.png" class="normal_model image-full-ab top-[7%] right-[-2%] z-[-1]" style="
+    <img src="/images/vector6.png" class="normal_model image-full-ab top-[7%] right-[-2%] z-[-1]" style="
     width:46vw;
    " />
 
-   
-<teleport to="body">
-    <div v-if="isModalOpen" class="modal-overlay" @click="closeModel">
-      <p class="text-[2vw] text-white SF-TH p-[5%] absolute j-center top-1 w-full">
-        ตัวอย่างโมเดลอวัยวะ "{{ selectedModelHeader }}"
-      </p>
 
-      <div class="modal-content" @click.stop>
-        <div v-if="isLoading" class="flex items-center justify-center w-full">
-          <div class="relative w-full">
-            <div class="progress-bar">
+    <teleport to="body">
+      <div v-if="isModalOpen" class="modal-overlay" @click="closeModel">
+        <p class="text-[2vw] text-white SF-TH p-[5%] absolute j-center top-1 w-full">
+          ตัวอย่างโมเดลอวัยวะ "{{ selectedModelHeader }}"
+        </p>
+
+        <div class="modal-content" @click.stop>
+          <div v-if="isLoading" class="flex items-center justify-center w-full">
+            <div class="relative w-full">
+              <div class="progress-bar">
+                <div class="progress-fill" :style="{ width: loadingPercent + '%' }" transition="width 0.1s linear">
+                </div>
+              </div>
               <div
-                class="progress-fill"
-                :style="{ width: loadingPercent + '%' }"
-                transition="width 0.1s linear"
-              ></div>
-            </div>
-            <div class=" mt-[5%] px-10 py-3 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
-              loading... {{ loadingPercent }}%
+                class=" mt-[5%] px-10 py-3 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
+                loading... {{ loadingPercent }}%
+              </div>
             </div>
           </div>
-        </div>
 
-        <div v-else>
-          <div
-            v-for="(model, index) in selectedModels"
-            :key="model"
-            :style="{ 'animation-duration': '4s', 'animation-delay': `${index * 0.8}s` }"
-            class="animate__animated animate__zoomIn"
-          >
-            <component :is="getIconComponent(model)" class="" />
+          <div v-else>
+            <div v-for="(model, index) in selectedModels" :key="model"
+              :style="{ 'animation-duration': '4s', 'animation-delay': `${index * 0.8}s` }"
+              class="animate__animated animate__zoomIn">
+              <component :is="getIconComponent(model)" class="" />
+            </div>
           </div>
         </div>
-      </div>
-      <button
-          @click="closeModel"
-          type="button"
-          class="modal-close absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-        >
-          <svg
-            class="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
+        <button @click="closeModel" type="button"
+          class="modal-close absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
           </svg>
           <span class="sr-only">Close modal</span>
         </button>
-    </div>
-  </teleport>
+      </div>
+    </teleport>
 
 
 
@@ -247,21 +208,21 @@ const detailCard = ref([
   {
     header: "หัวใจ",
     desc: "เป็นอวัยวะกล้ามเนื้อซึ่งสูบเลือดทั่วหลอดเลือดไปยังส่วนต่างๆของร่างกายโดยการหดตัวเป็นจังหวะซ้ำ ๆ พบในสัตว์ทุกชนิดที่มีระบบไหลเวียนซึ่งรวมสัตว์มีกระดูกสันหลังด้วยหัวใจสัตว์มีกระดูกสันหลังนั้นประกอบด้วยกล้ามเนื้อหัวใจ และเนื้อเยื่อเกี่ยวพันเป็นหลัก กล้ามเนื้อหัวใจเป็นกล้ามเนื้อลายที่อยู่นอกเหนืออำนาจจิตใจ พบเฉพาะที่หัวใจ และทำให้หัวใจสามารถสูบเลือดได้",
-    img: '/images/heart.svg',  
-    models: ['Heart']  
+    img: '/images/heart.svg',
+    models: ['Heart']
   },
   {
     header: "ตับ",
     desc: "เป็นอวัยวะที่มีขนาดใหญ่ที่สุดในช่องท้อง อยู่ใต้กระดูกซี่โครงบริเวณชายโครงขวาเลยมาถึงลิ้นปี่ ปกติกล้ามเนื้อตับจะมีสีแดง หนักประมาณ 1.5 กิโลกรัม ในแต่ละวันเลือดในร่างกายของคนเราซึ่งมีอยู่ราวๆ 5 ลิตรจะไหลผ่านตับรอบแล้วรอบเล่าถึง 360 รอบ ซึ่งหากวัดปริมาณเลือดที่ผ่านตับก็จะมากถึงวันละ 1,800 ลิตรเลยทีเดียว",
     img: '/images/liver.svg',
-    models: ['Liver']   
-     },
+    models: ['Liver']
+  },
   {
     header: "สมอง",
     desc: "เป็นอวัยวะที่มีความซับซ้อนที่ควบคุมความคิด ความจำ อารมณ์ การสัมผัส ทักษะด้านการเคลื่อนไหว การมองเห็น การหายใจ การควบคุมอุณหภูมิ ความหิว และกระบวนการอื่นๆในการควบคุมร่างกาย และสมองร่วมกับไขสันหลังที่รวมเรียกว่าระบบประสาทส่วนกลาง",
     img: '/images/brain.svg',
-    models: ['Brain']   
-     },
+    models: ['Brain']
+  },
 ]);
 
 // ฟังก์ชันเพื่อคืนพาธของรูปภาพ
@@ -289,25 +250,28 @@ const getIconComponent = (model) => {
 </script>
 
 <style scoped>
-
 .modal-content {
   position: relative;
 }
 
 .progress-bar {
-  height: 4px; /* ปรับความสูงตามต้องการ */
-  background-color: #e0e0e0; /* สีพื้นหลัง */
+  height: 4px;
+  /* ปรับความสูงตามต้องการ */
+  background-color: #e0e0e0;
+  /* สีพื้นหลัง */
 }
 
 .progress-fill {
   height: 100%;
-  background-color: #3b82f6; /* สีของ progress */
-  transition: width 0.1s linear; /* เวลาเปลี่ยนของแถบ */
+  background-color: #3b82f6;
+  /* สีของ progress */
+  transition: width 0.1s linear;
+  /* เวลาเปลี่ยนของแถบ */
 }
 
 /* Modal overlay - covers the entire screen */
 .lock-scroll {
-    overflow: hidden;
+  overflow: hidden;
 }
 
 .modal-overlay {
@@ -316,7 +280,8 @@ const getIconComponent = (model) => {
   left: 0;
   width: 100vw;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.958); /* Gray background */
+  background: rgba(0, 0, 0, 0.958);
+  /* Gray background */
   display: flex;
   justify-content: center;
   align-items: center;
@@ -332,21 +297,25 @@ const getIconComponent = (model) => {
   line-height: 1.5em;
   background: transparent;
   border: none;
-  scrollbar-width: none; /* สำหรับ Firefox */
+  scrollbar-width: none;
+  /* สำหรับ Firefox */
 }
 
 .scrollable-text::-webkit-scrollbar {
-  display: none; /* สำหรับ Chrome, Safari */
+  display: none;
+  /* สำหรับ Chrome, Safari */
 }
 
 .flex-container {
   display: flex;
   flex-direction: column;
 }
-.small_model{
+
+.small_model {
   display: none;
 }
-.normal_model{
+
+.normal_model {
   display: flex;
 }
 
@@ -371,11 +340,13 @@ const getIconComponent = (model) => {
     /* ทำให้ตัวอักษรเป็นตัวใหญ่ */
     width: 80%;
   }
-  .small_model{
+
+  .small_model {
     display: flex;
   }
-  .normal_model{
-    display : none;
+
+  .normal_model {
+    display: none;
   }
 
   .card-container {
@@ -403,7 +374,4 @@ const getIconComponent = (model) => {
   width: 50%;
   height: auto;
 }
-
-
-
 </style>
