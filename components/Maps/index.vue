ccb07a6acf49c967eba6f395b34ab53912e4a181
jajerrr <107903870+jajerrr@users.noreply.github.com>
2024-12-17 13:32:07 +0700

    Update index.vue

---
+++ components/Maps/index.vue
@@ -0,0 +1,14 @@
+<template>
+ 
+ <div class="w-full h-screen bg-[url(~/assets/images/prod-main-login-1.jpg)] bg-no-repeat bg-cover bg-center object-cover  ">
+        
+                <div  class="absolute w-full  bottom-0  left-0">
+                  <CardsCardCoverflow />
+                </div>
+                  
+                
+            
+    </div>
+ 
+</template>
+ 
