a0877e50bddda6d9c65f3be47d3af0c2360598a1
jajerrr <107903870+jajerrr@users.noreply.github.com>
2025-01-21 18:28:40 +0700

    ..simulayout

--- components/Simulator/index.vue
+++ components/Simulator/index.vue
@@ -1,49 +1,34 @@
 <template>
   <NuxtLayout name="bgsimulator">
-    <div class="w-full h-screen absolute flex justify-center items-center z-30">
-      <div
-        class="w-full h-screen absolute flex justify-center items-center z-30"
-      >
-        <img src="assets/images/simulator/cardsimu.png" alt="" class="" />
-      </div>
-
-      <div class="absolute flex justify-center items-center z-40">
-        <div class="flex-col gap-5 flex w-1/2 items-center text-white SF-TH">
-          <h1 class="text-3xl font-bold">เกมหัตถการที่ 1</h1>
-          <h2 class="neon text-3xl text-center font-bold">
-            Advance Cardio-Pulmonary <br />
-            Resuscitation
-          </h2>
-          <img src="assets/images/simulator/linesimu.png" alt="" />
-          <h3 class="text-xl text-center">
-            ประเมินสถานการณ์ เพื่อฟื้นคืนชีพขั้นพื้นฐาน<br />ใช้เครื่องมือ
-            Defibrillator กระตุ้นหัวใจผู้ป่วย
-            <br />และใช้การกระตุ้นหัวใจด้วยไฟฟ้าหรือยาแก่ผู้ป่วย
-          </h3>
-          <div>
-            <div class="absolute flex items-center z-10"
-            style="margin-left:15px;
+    <div class="absolute flex justify-center items-center z-40">
+      <div class="flex-col gap-5 flex w-1/2 items-center text-white SF-TH">
+        <h1 class="text-3xl font-bold">เกมหัตถการที่ 1</h1>
+        <h2 class="neon text-3xl text-center font-bold">
+          Advance Cardio-Pulmonary <br />
+          Resuscitation
+        </h2>
+        <img src="assets/images/simulator/linesimu.png" alt="underluine" />
+        <h3 class="text-xl text-center">
+          ประเมินสถานการณ์ เพื่อฟื้นคืนชีพขั้นพื้นฐาน<br />ใช้เครื่องมือ
+          Defibrillator กระตุ้นหัวใจผู้ป่วย
+          <br />และใช้การกระตุ้นหัวใจด้วยไฟฟ้าหรือยาแก่ผู้ป่วย
+        </h3>
+        <div>
+          <div class="absolute flex items-center z-10" style="margin-left:15px;
             margin-top:5px;">
-              <button
-                type="button"
-                class="rounded-lg flex items-center space-x-[1rem] bg-[#FF7A00] hover:bg-[#ffe1c5] px-6 py-2 transition ease-in-out"
-              >
-                <img src="assets/images/simulator/iconsimu.png" alt="" />
-                <span class="text-nm text-white">Simulator</span>
-              </button>
-            </div>
-            <div class="">
-              <img
-                src="assets/images/simulator/bt-line.png  "
-                alt=""
-                class=""
-              />
-            </div>
+            <button type="button"
+              class="rounded-lg flex items-center space-x-[1rem] bg-[#FF7A00] hover:bg-[#ffe1c5] px-6 py-2 transition ease-in-out">
+              <img src="assets/images/simulator/iconsimu.png" alt="" />
+              <span class="text-nm text-white">Simulator</span>
+            </button>
+          </div>
+          <div class="">
+            <img src="assets/images/simulator/bt-line.png  " alt="button-frame" class="" />
           </div>
         </div>
-        <div class="flex w-1/2">
-          <img src="assets/images/simulator/CPR.png" alt="" class="" />
-        </div>
+      </div>
+      <div class="flex w-1/2">
+        <img src="assets/images/simulator/CPR.png" alt="image-type" class="" />
       </div>
     </div>
   </NuxtLayout>
--- layouts/bgsimulator.vue
+++ layouts/bgsimulator.vue
@@ -1,41 +1,46 @@
 <template>
     <div class="bg-map  bg-mobile">
-     <img src="~/assets/images/simulator/bgsimu.jpg" alt="" class="max-h-screen max-w-none overflow-x-scroll hide">
-     
-                 
-                     <slot></slot>
-                 
-             
-     </div>
- </template>
- 
- <script setup>
- 
- </script>
- 
- <style lang="scss" scoped>
- .bg-map{
-     @apply bg-[url(~/assets/images/simulator/bgsimu.jpg)] bg-no-repeat bg-cover bg-center object-cover w-screen h-screen ;
- }
- .hide{
-     display: none;
- }
- @media (max-width: 1024px) {
-     .bg-map{
-         display: none;
-     }
-     .bg-mobile{
-         display: block;
-         width: 100%;
-         height: 100dvh;
-         
-     }
-     .hide{
-     display: block;
- }
-     
- }
- 
- 
- 
- </style>
+        <img src="~/assets/images/simulator/bgsimu.jpg" alt="" class="max-h-screen max-w-none overflow-x-scroll hide">
+
+        <div class="w-full h-screen absolute flex justify-center items-center z-30">
+            <div class="w-full h-screen absolute flex justify-center items-center z-30">
+                <img src="assets/images/simulator/cardsimu.png" alt="" class="" />
+            </div>
+
+            <slot></slot>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.bg-map {
+    @apply bg-[url(~/assets/images/simulator/bgsimu.jpg)] bg-no-repeat bg-cover bg-center object-cover w-screen h-screen;
+}
+
+.hide {
+    display: none;
+}
+
+@media (max-width: 1024px) {
+    .bg-map {
+        display: none;
+    }
+
+    .bg-mobile {
+        display: block;
+        width: 100%;
+        height: 100dvh;
+
+    }
+
+    .hide {
+        display: block;
+    }
+
+}
+</style>
